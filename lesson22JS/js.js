28858520f33a29a4df043d5a20bf53edba105214
KhomyakDmytro <Khomyakdmytro@gmail.com>
2020-01-15 18:53:07 +0200

    exam2 and last changes

---
+++ lesson22JS/js.js
@@ -0,0 +1,32 @@
+// let day = Number(prompt('Enter day'));
+// let month = Number(prompt('Enter month'));
+// let year = Number(prompt('Enter year'));
+
+// let inputFirst = document.getElementById('numberone');
+// let inputTo = document.getElementById('numbertwo');
+// let button = document.getElementById('btn');
+// let sum = 0;
+// button.addEventListener('click', function () {
+//     // var sum = Number(inputFirst.value) + Number(inputTo.value);
+//     // alert(sum);
+//     let i = Number(inputFirst.value);
+//     while (i <= Number(inputTo.value)) {
+//         sum = sum + i;
+//         i = i + 1;
+//     }
+//     alert(sum);
+// });
+
+// var a = Number(prompt("Enter number"));
+// var b = Number(prompt("Enter number"));
+// var d = 2;
+// d = (a > b ? b : a);
+// // if (a % d === 0 && b % d === 0);
+// while (a > 2 && b >= 2) {
+//     if (a % d === 0 && b % d === 0) {
+//         break;
+//     }
+//     d++;
+// }
+// alert(d);
+
---
+++ test/jscript.js
@@ -0,0 +1,78 @@
+// let a = Number(prompt('Enter current time?'));
+// if (a > 7) {
+//     alert('Go Home');
+// }
+// else if (a > 4) {
+//     alert('Buy a Coffee and relax')
+// }
+// else {
+//     alert('Welcome');
+// }
+// 
+
+
+
+// let a = Number(prompt('enter first number'));
+// let calc = prompt('operator');
+// let b = Number(prompt('enter second number'));
+
+// if (calc === '+') {
+//     alert(a + b);
+// }
+// if (calc === '-') {
+//     alert(a - b);
+// }
+// if (calc === '*') {
+//     alert(a * b);
+// }
+// if (calc === '/') {
+//     alert(a / b);
+// }
+
+// let a = prompt('2+5?');
+// let b = prompt('3+6?');
+// let c = prompt('4+8?');
+// let result = 0;
+
+// if (a == 7) {
+//     result++;
+// }
+// if (b == 9) {
+//     result++;
+// }
+// if (c == 12) {
+//     result++;
+// }
+// alert(result);
+
+// let value = Number(prompt('value?'));
+// let currency = prompt('value?');
+// let result = 0;
+// if (currency === 'UAH') {
+//     result = value * 24;
+// }
+// else if (currency === 'EUR') {
+//     result = value * 1.2;
+// }
+
+// switch (currency) {
+//     case 'UAH':
+//         result = value * 24;
+//         break;
+//     case 'EUR':
+//     case 'GBP':
+//         result = value * 1.2;
+//         break;
+//     default:
+//         alert('no data');
+// }
+// alert(result);
+
+// let a = 0;
+// let result = 1;
+// while (a < 100) {
+//     result = result + a;
+//     a = a + 1;
+//     console.log(result);
+// }
+
